4a3be36995c00b6275c1d540d6296e90472d8753
swoook <kimseongwoo21@gmail.com>
2021-12-16 00:08:54 +0900

    Implementig soft-NMS (in progress)

--- torchvision/csrc/ops/cpu/soft_nms_kernel.cpp
+++ torchvision/csrc/ops/cpu/soft_nms_kernel.cpp
@@ -7,11 +7,11 @@ namespace ops {
 namespace {
 
 template <typename scalar_t>
-at::Tensor soft_nms_kernel_impl(
+std::tuple<at::Tensor, at::Tensor> soft_nms_kernel_impl(
     const at::Tensor& dets,
-    const at::Tensor& scores,
+    at::Tensor& scores,
     double sigma,
-    double iou_threshold) {
+    double score_threshold) {
   TORCH_CHECK(!dets.is_cuda(), "dets must be a CPU tensor");
   TORCH_CHECK(!scores.is_cuda(), "scores must be a CPU tensor");
   TORCH_CHECK(
@@ -31,58 +31,64 @@ at::Tensor soft_nms_kernel_impl(
   auto order_t = std::get<1>(scores.sort(0, /* descending=*/true));
 
   auto ndets = dets.size(0);
-  at::Tensor suppressed_t = at::zeros({ndets}, dets.options().dtype(at::kByte));
-  at::Tensor keep_t = at::zeros({ndets}, dets.options().dtype(at::kLong));
+  // at::Tensor suppressed_t = at::zeros({ndets}, dets.options().dtype(at::kByte));
+  // at::Tensor keep_t = at::zeros({ndets}, dets.options().dtype(at::kLong));
 
-//   auto suppressed = suppressed_t.data_ptr<uint8_t>();
-  auto keep = keep_t.data_ptr<int64_t>();
+  // auto suppressed = suppressed_t.data_ptr<uint8_t>();
+  // auto keep = keep_t.data_ptr<int64_t>();
   auto order = order_t.data_ptr<int64_t>();
   auto x1 = x1_t.data_ptr<scalar_t>();
   auto y1 = y1_t.data_ptr<scalar_t>();
   auto x2 = x2_t.data_ptr<scalar_t>();
   auto y2 = y2_t.data_ptr<scalar_t>();
+  auto scores_ptr = scores.data_ptr<scalar_t>();
   auto areas = areas_t.data_ptr<scalar_t>();
 
-  int64_t num_to_keep = 0;
+  // int64_t num_to_keep = 0;
 
   for (int64_t _i = 0; _i < ndets; _i++) {
     auto i = order[_i];
     // if (suppressed[i] == 1)
     //   continue;
-    keep[num_to_keep++] = i;
+    // keep[num_to_keep++] = i;
     auto ix1 = x1[i];
     auto iy1 = y1[i];
     auto ix2 = x2[i];
     auto iy2 = y2[i];
-    auto iscore = scores[i]
     auto iarea = areas[i];
 
     for (int64_t _j = _i + 1; _j < ndets; _j++) {
       auto j = order[_j];
-    //   if (suppressed[j] == 1)
-    //     continue;
+      // if (suppressed[j] == 1)
+      //   continue;
       auto xx1 = std::max(ix1, x1[j]);
       auto yy1 = std::max(iy1, y1[j]);
       auto xx2 = std::min(ix2, x2[j]);
       auto yy2 = std::min(iy2, y2[j]);
+      auto sscore = scores_ptr[j]
+
 
       auto w = std::max(static_cast<scalar_t>(0), xx2 - xx1);
       auto h = std::max(static_cast<scalar_t>(0), yy2 - yy1);
       auto inter = w * h;
       auto ovr = inter / (iarea + areas[j] - inter);
-      if (ovr > iou_threshold)
-        // suppressed[j] = 1;
-        
+      scores_ptr[j] *= torch::exp(-(ovr * ovr) / sigma)
+      // if (ovr > iou_threshold)
+      //   suppressed[j] = 1;
     }
   }
+
+  auto keep = scores.index({scores_updated > score_threshold, 4});
+  keep = keep.to(torch::kLong);
+
   return keep_t.narrow(/*dim=*/0, /*start=*/0, /*length=*/num_to_keep);
 }
 
-at::Tensor soft_nms_kernel(
+std::tuple<at::Tensor, at::Tensor> soft_nms_kernel(
     const at::Tensor& dets,
-    const at::Tensor& scores,
+    at::Tensor& scores,
     double sigma,
-    double iou_threshold) {
+    double score_threshold) {
   TORCH_CHECK(
       dets.dim() == 2, "boxes should be a 2d tensor, got ", dets.dim(), "D");
   TORCH_CHECK(
@@ -105,9 +111,9 @@ at::Tensor soft_nms_kernel(
   auto result = at::empty({0}, dets.options());
 
   AT_DISPATCH_FLOATING_TYPES(dets.scalar_type(), "soft_nms_kernel", [&] {
-    result = soft_nms_kernel_impl<scalar_t>(dets, scores, sigma, iou_threshold);
+    result = soft_nms_kernel_impl<scalar_t>(dets, scores, sigma, score_threshold);
   });
-  return result;
+  return std::make_tuple(scores, result);
 }
 
 } // namespace
